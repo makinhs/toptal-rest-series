e2b3ddd5e8b2cb1e61f88350d8571df51d43bee6
Marcos Silva <tio.makin@gmail.com>
2020-10-07 12:37:53 +0200

    update(): comment update suggestions and npm scripts

--- app.ts
+++ app.ts
@@ -10,7 +10,7 @@ import {UsersRoutes} from './users/users.routes.config';
 
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
-const port = 3000;
+const port = 3010;
 const routes: any = [];
 const debug = require('debug')('app');
 
--- users/users.routes.config.ts
+++ users/users.routes.config.ts
@@ -19,20 +19,21 @@ export class UsersRoutes extends CommonRoutesConfig implements configureRoutes {
 
         this.app.route(`/users/:userId`)
             .all((req: express.Request, res: express.Response, next: express.NextFunction) => {
-                // we can handle a middleware that needs to exist to all the other verbs for this endpoint
+                // This middleware function runs before any request to /users/:userId
+                // It doesn't accomplish anything just yet---it simply passes control to the next applicable function below using next()
                 next();
             })
             .get((req: express.Request, res: express.Response) => {
-                res.status(200).send(`Get to ${req.params.userId}`);
+                res.status(200).send(`GET requested for id ${req.params.userId}`);
             })
             .put((req: express.Request, res: express.Response) => {
-                res.status(200).send(`Put to ${req.params.userId}`);
+                res.status(200).send(`Put requested for id ${req.params.userId}`);
             })
             .patch((req: express.Request, res: express.Response) => {
-                res.status(200).send(`Patch to ${req.params.userId}`);
+                res.status(200).send(`Patch requested for id ${req.params.userId}`);
             })
             .delete((req: express.Request, res: express.Response) => {
-                res.status(200).send(`Delete to ${req.params.userId}`);
+                res.status(200).send(`Delete requested for id ${req.params.userId}`);
             });
     }
 }
