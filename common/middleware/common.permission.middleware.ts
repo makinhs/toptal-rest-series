8736d05495f6a15bf3271a45ff52a18450660dd7
Kevin Bloch <kev@codingthat.com>
2021-02-05 16:15:35 +0100

    Add permission level enum
    Avoid having to maintain duplicate lists.

--- common/middleware/common.permission.middleware.ts
+++ common/middleware/common.permission.middleware.ts
@@ -1,16 +1,8 @@
+import { PermissionLevel } from './common.permissionlevel.enum';
 class CommonPermissionMiddleware {
 
-    static FREE_PERMISSION = 1;
-    static PAID_PERMISSION = 2;
-    static ANOTHER_PAID_PERMISSION = 4;
-    static ADMIN_PERMISSION = 8;
-    static SUDO_PERMISSION = 2147483647;
 
     private static instance: CommonPermissionMiddleware;
-    FREE_PERMISSION = CommonPermissionMiddleware.FREE_PERMISSION;
-    PAID_PERMISSION = CommonPermissionMiddleware.PAID_PERMISSION;
-    ANOTHER_PAID_PERMISSION = CommonPermissionMiddleware.ANOTHER_PAID_PERMISSION;
-    SUDO_PERMISSION = CommonPermissionMiddleware.SUDO_PERMISSION;
 
     static getInstance() {
         if (!CommonPermissionMiddleware.instance) {
@@ -19,11 +11,11 @@ class CommonPermissionMiddleware {
         return CommonPermissionMiddleware.instance;
     }
 
-    minimumPermissionLevelRequired(requiredPermissionLevel: any) {
+    minimumPermissionLevelRequired(requiredPermissionLevel: PermissionLevel) {
         return (req: any, res: any, next: any) => {
             try {
                 let userPermissionLevel = parseInt(req.jwt.permissionLevel);
-                if (userPermissionLevel & Number.parseInt(requiredPermissionLevel)) {
+                if (userPermissionLevel & requiredPermissionLevel) {
                     next();
                 } else {
                     res.status(403).send({});
@@ -41,7 +33,7 @@ class CommonPermissionMiddleware {
         if (req.params && req.params.userId && userId === req.params.userId) {
             return next();
         } else {
-            if (userPermissionLevel & CommonPermissionMiddleware.ADMIN_PERMISSION) {
+            if (userPermissionLevel & PermissionLevel.ADMIN_PERMISSION) {
                 return next();
             } else {
                 return res.status(403).send({});
@@ -51,7 +43,7 @@ class CommonPermissionMiddleware {
 
     async onlyAdminCanDoThisAction(req: any, res: any, next: any) {
         let userPermissionLevel = parseInt(req.jwt.permissionLevel);
-        if (userPermissionLevel & CommonPermissionMiddleware.ADMIN_PERMISSION) {
+        if (userPermissionLevel & PermissionLevel.ADMIN_PERMISSION) {
             return next();
         } else {
             return res.status(403).send({});
