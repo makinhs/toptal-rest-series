4f72795945e7ba92c0f67782b044a6db46f3251e
Kevin Bloch <kev@codingthat.com>
2021-03-15 11:15:18 +0100

    Don't expose auth error information

--- auth/controllers/auth.controller.ts
+++ auth/controllers/auth.controller.ts
@@ -1,7 +1,9 @@
 import express from 'express';
-
+import debug from 'debug';
 import jwt from 'jsonwebtoken';
 import crypto from 'crypto';
+
+const log: debug.IDebugger = debug('app:auth-controller');
 
 // @ts-expect-error
 const jwtSecret:string = process.env.JWT_SECRET;
@@ -20,7 +22,8 @@ class AuthController {
             const token = jwt.sign(req.body, jwtSecret, {expiresIn: tokenExpirationInSeconds});
             return res.status(201).send({accessToken: token, refreshToken: hash});
         } catch (err) {
-            return res.status(500).send(err);
+            log('createJWT error: %O', err);
+            return res.status(500).send();
         }
     }
 }
